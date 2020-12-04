0fccf222535550cfd9b267fb7ee65d2a295a2b53
AnnaKatunina <leznevoanna@gmail.com>
2020-12-04 18:58:13 +0000

    Commit wsgi

--- vacancies_project/wsgi.py
+++ vacancies_project/wsgi.py
@@ -1,16 +1,10 @@
-"""
-WSGI config for vacancies_project project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
-"""
-
 import os
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vacancies_project.settings')
 
 application = get_wsgi_application()
+application = WhiteNoise(application, root='/path/to/static/files')
+application.add_files('/path/to/more/static/files', prefix='more-files/')
